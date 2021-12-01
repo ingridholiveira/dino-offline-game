f91b584dc07598c37edd0ca2236e05967fd3eb2e
Ingridh Nunes <ingridhnunes@gmail.com>
2021-12-01 10:07:26 -0300

    botoes

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ function createCactus() {
       // Game over
       clearInterval(leftTimer);
       isGameOver = true;
-      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1> <button class="btn">Jogar Novamente</button>';
     } else {
       cactusPosition -= 10;
       cactus.style.left = cactusPosition + 'px';
